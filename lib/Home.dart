61579c78fbca12621f0c40921d4236f5cb29f047
nikipatel385 <nikipatel385@gmail.com>
2020-01-06 17:16:45 +0530

    Initial commit

--- lib/Home.dart
+++ lib/Home.dart
@@ -14,14 +14,13 @@ class home extends StatefulWidget {
 }
 
 class _homeState extends State<home> {
-
   static final String url = 'http://172.16.17.141/app_teacher_school_list.php';
   static final auth = 'Trizinno2019';
   List data;
 
   Future<String> getJSONdata() async {
     var response =
-    await http.get(Uri.encodeFull(url), headers: {"auth": "Trizinno2019"});
+        await http.get(Uri.encodeFull(url), headers: {"auth": "Trizinno2019"});
     print(response.body);
 
     setState(() {
@@ -58,16 +57,14 @@ class _homeState extends State<home> {
               child: Column(
                 children: <Widget>[
                   TypeAheadFormField(
-
                     textFieldConfiguration: TextFieldConfiguration(
                       autofocus: true,
                       controller: this._typeAheadController,
-
                       decoration:
                           InputDecoration(hintText: 'Select Your School'),
                     ),
                     debounceDuration: const Duration(milliseconds: 300),
-                    suggestionsCallback: (pattern)  {
+                    suggestionsCallback: (pattern) {
                       return data;
                     },
                     itemBuilder: (context, suggestion) {
@@ -77,7 +74,8 @@ class _homeState extends State<home> {
                       return suggestionsBox;
                     },
                     onSuggestionSelected: (suggestion) {
-                      this._typeAheadController.text = suggestion['SCHOOL_NAME'];
+                      this._typeAheadController.text =
+                          suggestion['SCHOOL_NAME'];
                     },
                     // ignore: missing_return
                     validator: (value) {
